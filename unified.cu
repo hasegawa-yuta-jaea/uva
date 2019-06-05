758233bdbcc7b3fa3fadf96f895856066897be56
hasegawa <hasegawa.sim.gsic.titech@gmail.com>
2019-06-05 19:12:22 +0900

    cudaMemAdvise

--- unified.cu
+++ unified.cu
@@ -11,7 +11,7 @@
 #include "util/signal.hpp"
 
 const int num_gpu = 16;
-const long elem = (num_gpu *1024l*1024l*1024l /8) *16; // weak scaling in GiB
+const long elem = (num_gpu *1024l*1024l*1024l /8) *32; // weak scaling in GiB
 const int iter = 4;
 
 const int nth = 1024;
@@ -19,21 +19,24 @@ const long grid = elem/nth;
 const long block = nth;
 
 __device__ __forceinline__ long index(const int gpu) { 
-  return threadIdx.x + blockDim.x * (blockIdx.x + gridDim.x*gpu*0); // gridDim.x*gpu in nvidia document, but slow when total mem > 32 GB??
+  return threadIdx.x + blockDim.x * (blockIdx.x + gridDim.x*gpu); // gridDim.x*gpu needs cudaMemAdvise() and cudaMemPrefetchAsync(), otherwise get very slow
 }
 
 __device__ __forceinline__ long index_boundary(const long idx, const int gpu) {
-  if(gpu == 0 && idx < 0) return idx + elem;
-  else if(gpu == num_gpu-1 && idx >= elem/num_gpu) return idx - elem;
-  else return idx;
+  //if(gpu == 0 && idx < 0) return idx + elem;
+  //else if(gpu == num_gpu-1 && idx >= elem/num_gpu) return idx - elem;
+  //else return idx;
+  if(idx < 0) return idx + elem;
+  else if(idx >= elem) return idx - elem;
+  return idx;
 }
 
-__global__ void init(float* dst, float* src, const long ofs, const int gpu) {
+__global__ void init(float* dst, float* src, const int gpu) {
   const long k = index(gpu);
   dst[k] = src[k] = k;
 }
 
-__global__ void foo(float *const dst, const float *const src, const long ofs, const int gpu) {
+__global__ void foo(float *const dst, const float *const src, const int gpu) {
   const long idx = index(gpu);
   //// stream test
   //dst[idx] = src[idx];
@@ -43,8 +46,8 @@ __global__ void foo(float *const dst, const float *const src, const long ofs, co
   //const long ip = index_boundary(idx+1, gpu);
   //dst[idx] = (1.f - 2.f*c)*src[idx] + c*(src[im] + src[ip]);
   // 1D shift test
-  const long far = 1;
-  const long isx = index_boundary(idx-far);
+  const long far = elem/num_gpu;
+  const long isx = index_boundary(idx-far, gpu);
   dst[idx] = src[isx];
 }
 
@@ -55,8 +58,21 @@ int main(int argc, char** argv) try {
 
   std::cout << "step: malloc-gpu" << std::endl;
   timer.elapse("malloc-gpu", [&]() {
-    cudaMallocManaged(&dst, elem * sizeof(float));
-    cudaMallocManaged(&src, elem * sizeof(float));
+    const size_t memall = elem * sizeof(float);
+    const size_t memgpu = memall / num_gpu;
+    cudaMallocManaged(&dst, memall);
+    cudaMallocManaged(&src, memall);
+    for(int i=0; i<num_gpu; i++) {
+      cudaMemAdvise(dst, memall, cudaMemAdviseSetAccessedBy, i);
+      cudaMemAdvise(src, memall, cudaMemAdviseSetAccessedBy, i);
+    }
+    for(int i=0; i<num_gpu; i++) {
+      const size_t ofs = elem*i/num_gpu;
+      cudaMemAdvise(dst + ofs, memgpu, cudaMemAdviseSetPreferredLocation, i);
+      cudaMemAdvise(src + ofs, memgpu, cudaMemAdviseSetPreferredLocation, i);
+      cudaMemPrefetchAsync(dst + ofs, memgpu, i);
+      cudaMemPrefetchAsync(src + ofs, memgpu, i);
+    }
   });
 
   if(std::numeric_limits<int>::max() < elem) { 
@@ -67,12 +83,20 @@ int main(int argc, char** argv) try {
   std::cout << "total mem = " << 2l*elem*sizeof(float) / 1024/1024/1024 << " GiB" << std::endl;
   std::cout << "  per gpu = " << 2l*elem*sizeof(float) / 1024/1024/1024./num_gpu << " GiB" << std::endl;
 
+  std::cout << std::endl;
+  for(int i=0; i<num_gpu; i++) {
+    size_t mfree, mtotal;
+    CUDA_SAFE_CALL(cudaSetDevice(i));
+    CUDA_SAFE_CALL(cudaMemGetInfo(&mfree, &mtotal));
+    std::cout << "gpu " << std::setw(2) << std::setfill(' ') << i << ": "
+      << std::setw(4) << double(mtotal - mfree) /1024./1024./1024. << " GiB used" << std::endl;
+  }
+
   std::cout << "step: init" << std::endl;
   timer.elapse("init-gpu", [&]() {
     for(int i=0; i<num_gpu; i++) {
       cudaSetDevice(i);
-      const long ofs = elem*i/num_gpu; // ofs = 0 in nvidia's document, but slow when total mem > 32 GB??
-      init<<<grid/num_gpu, block>>>(dst + ofs, src + ofs, ofs, i);
+      init<<<grid/num_gpu, block>>>(dst, src, i);
     }
     for(int i=0; i<num_gpu; i++) {
       cudaSetDevice(i);
@@ -100,8 +124,7 @@ int main(int argc, char** argv) try {
           dst = tmp;
           for(int i=0; i<num_gpu; i++) {
             cudaSetDevice(i);
-            const long ofs = elem*i/num_gpu; // ofs = 0 in nvidia's document, but slow when total mem > 32 GB??
-            foo<<<grid/num_gpu, block>>>(dst + ofs, src + ofs, ofs, i);
+            foo<<<grid/num_gpu, block>>>(dst, src, i);
           }
           for(int i=0; i<num_gpu; i++) {
             cudaSetDevice(i);
