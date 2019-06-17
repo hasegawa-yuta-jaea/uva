4c20f02ff4efc28f06048f1420739ef9a21a2d0b
hasegawa <hasegawa.sim.gsic.titech@gmail.com>
2019-06-17 16:06:56 +0900

    branch efficiency (detect ward divergence?)

--- prof.sh
+++ prof.sh
@@ -5,7 +5,8 @@ nvprof \
 dram_read_bytes,dram_write_bytes,flop_count_sp,inst_integer\
 ,gld_efficiency,gst_efficiency\
 ,l2_global_load_bytes,\
-,global_hit_rate\
+,global_hit_rate,\
+,branch_efficiency\
  --events \
 global_load,global_store\
 ,l2_subp0_total_read_sector_queries,l2_subp1_total_read_sector_queries\
